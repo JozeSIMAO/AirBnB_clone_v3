811f3d455d4ed87825df3399b2400b73b63997f6
JozeSIMAO <jozesimao10@gmail.com>
2024-01-08 14:31:30 +0200

    3 Status of your api

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,11 +1,46 @@
 #!/usr/bin/python3
-"""Create a route called status on the object app_views"""
+"""
+index
+"""
 
-from api.v1.views import app_views
 from flask import jsonify
+from api.v1.views import app_views
+
+from models import storage
 
 
-@app_views.route('/status', methods=['GET'])
-def get_status():
-    """Returns the status of the API"""
-    return jsonify({"status": "OK"})
+@app_views.route("/status", methods=['GET'], strict_slashes=False)
+def status():
+    """
+    status route
+    :return: response with json
+    """
+    data = {
+        "status": "OK"
+    }
+
+    resp = jsonify(data)
+    resp.status_code = 200
+
+    return resp
+
+
+@app_views.route("/stats", methods=['GET'], strict_slashes=False)
+def stats():
+    """
+    stats of all objs route
+    :return: json of all objs
+    """
+    data = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User"),
+    }
+
+    resp = jsonify(data)
+    resp.status_code = 200
+
+    return resp
